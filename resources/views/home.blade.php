67302d20f8386976b5d695bb6cae6ef6ac04e1b1
mystry22 <32303356+mystry22@users.noreply.github.com>
2022-07-17 10:03:19 -0700

    home page update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
   <div class="row">
     <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-      <h1>Let us plan your savings</h1>
+      <h1>Let's plan your savings</h1>
       <h2>We are team of talented Financial Experts that helps you plan your earnings</h2>
       <div class="d-flex">
         <a href="/signup" class="btn-get-started scrollto">Get Started</a>
